56fa16aca332258fb8a95611e2d73157dd03c9fa
sutwal123@gmail.com <sutwal123@gmail.com>
2025-10-19 05:12:47 +0530

    itemcard created

---
+++ Kisan Fresh/src/assets/MinusSvg.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MinusSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="w-6 h-6"
+    >
+      <path d="M5 12h14" />
+    </svg>
+  );
+}
+
+export default MinusSvg;
