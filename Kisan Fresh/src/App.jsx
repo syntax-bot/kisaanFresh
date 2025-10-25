2579789d41031bf62f6bdfa329d8c2f2309beeb4
sutwal123@gmail.com <sutwal123@gmail.com>
2025-10-26 04:44:58 +0530

    added theme and responsive itemcard

--- Kisan Fresh/src/App.jsx
+++ Kisan Fresh/src/App.jsx
@@ -6,13 +6,65 @@ function App() {
 
   return (
     <>
-      <ItemCard
-        name="banana"
-        oldPrice="50"
-        newPrice="30"
-        rating={4.2}
-        image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
-      />
+      
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          /><ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+          <ItemCard
+            name="banana"
+            oldPrice="50"
+            newPrice="30"
+            rating={4.2}
+            image="https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg"
+          />
+        </div>
+
     </>
   );
 }
